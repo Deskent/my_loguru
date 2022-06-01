2f835559ab95dba1e8115ef6f3368303484c1f62
deskent <battenetciz@gmail.com>
2022-06-01 12:08:16 +0300

    0.0.7
    fix default_log_level from 1 to 20
    add attr level to get_logger func
    upd version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='myloguru-deskent',
-    version='0.0.6',
+    version='0.0.7',
     author='Deskent',
     author_email='battenetciz@gmail.com',
     description='My loguru config',
